7eb499aabfea89dbe1af1d3b7c4e8847a575ffe3
EunsooPark <elfflqjem3366@naver.com>
2024-06-14 17:30:12 +0900

    작성일:2024-06-14 작성시간:17:28 작성자:박은수 메모:스프링 뼈대 생성

---
+++ pal/ksmart51team03/src/main/java/team03/Ksmart51team03Application.java
@@ -0,0 +1,13 @@
+package team03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksmart51team03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksmart51team03Application.class, args);
+	}
+
+}
---
+++ pal/ksmart51team03/src/test/java/team03/Ksmart51team03ApplicationTests.java
@@ -0,0 +1,13 @@
+package team03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksmart51team03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
