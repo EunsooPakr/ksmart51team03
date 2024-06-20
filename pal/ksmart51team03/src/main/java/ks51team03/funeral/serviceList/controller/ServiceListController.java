ed8233b90dd60c1c44d681ef40a666f1146a5eff
smartisong <wodlr1310@naver.com>
2024-06-21 01:00:53 +0900

    06.21 1차 커밋
    작성자 송재익
    
    작성내용 장례예약 메인페이지 수정

--- pal/ksmart51team03/src/main/java/ks51team03/funeral/serviceList/controller/ServiceListController.java
+++ pal/ksmart51team03/src/main/java/ks51team03/funeral/serviceList/controller/ServiceListController.java
@@ -1,5 +1,14 @@
 package ks51team03.funeral.serviceList.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ServiceListController {
 
+	@GetMapping("/funeral/serviceList")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	public String funeralServiceList() {
+
+		return "funeral/funeral_serviceList.html";
+	}
 }
