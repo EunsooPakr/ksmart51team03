1346df005549fca3f8a0374aacf3ab90949b3bbc
EunsooPark <elfflqjem3366@naver.com>
2024-06-20 11:26:25 +0900

    작성일:2024-06-20 작성시간:11:26 작성자:박은수 메모:스프링 뼈대에 송이&은수 연결

---
+++ pal/ksmart51team03/src/main/java/team03/map/controller/MapController.java
@@ -0,0 +1,15 @@
+package team03.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+	
+	 	@GetMapping("/map/main")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	    public String mainPage() {
+
+	        return "map/map_main";
+	    }
+
+}
