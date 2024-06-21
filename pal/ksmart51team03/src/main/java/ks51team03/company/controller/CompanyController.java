b8d864ef16fe6c4c1476159046572d1ecb788447
EunsooPark <elfflqjem3366@naver.com>
2024-06-21 17:15:59 +0900

    작성일:2024-06-21 작성시간:17:15 작성자:박은수 메모:맵, 업체 페이지 추가

--- pal/ksmart51team03/src/main/java/ks51team03/company/controller/CompanyController.java
+++ pal/ksmart51team03/src/main/java/ks51team03/company/controller/CompanyController.java
@@ -6,42 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CompanyController {
 	
-	@GetMapping("/company/insert")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_insert")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyInsert() {
 
-		return "company/company_insert";
+		return "company_modify";
 	}
-	@GetMapping("/company/info")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_info")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyInfo() {
 
 		return "company/company_info";
 	}
-	@GetMapping("/company/modify")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_modify")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyModify() {
 
 		return "company/company_modify";
 	}
-	@GetMapping("/company/delete")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
-	public String companyDelete() {
 
-		return "company/company_delete";
-	}
-	@GetMapping("/company/send_alarm")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_send_alarm")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companySendAlarm() {
 
 		return "company/company_send_alarm";
 	}
-	@GetMapping("/company/staff_setting")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_staff_setting")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyStaffSetting() {
 
 		return "company/company_staff_setting";
 	}
-	@GetMapping("/company/question")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_question")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyQuestion() {
 
 		return "company/company_question";
 	}
-	@GetMapping("/company/review")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
+	@GetMapping("/company/company_review")			// 어노테이션 괄호안에는 옵션을 쓴다.   /  컨트롤러에서는 무조건 String으로 반환
 	public String companyReview() {
 
 		return "company/company_review";
