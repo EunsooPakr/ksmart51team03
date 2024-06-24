31f44ac215e421e5c3cb3928b00c4a92d2046e1b
Chande1 <chandelier125@gmail.com>
2024-06-25 07:00:32 +0900

    작성자:한송이 작성자:2024-06-25 작성시간:07:00 내용:회원가입관련페이지

--- pal/ksmart51team03/src/main/java/ks51team03/member/controller/CommonController.java
+++ pal/ksmart51team03/src/main/java/ks51team03/member/controller/CommonController.java
@@ -13,4 +13,44 @@ public class CommonController {
 		
 		return "member/member_main";
 	}
+	
+	@GetMapping("/member/member_login_terms_mem")
+	public String userTermsPageMem(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "member/member_login_terms_mem";
+	}
+	
+	@GetMapping("/member/member_login_terms_com")
+	public String userTermsPageCom(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "member/member_login_terms_com";
+	}
+	
+	@GetMapping("/member/member_login_insert_mem")
+	public String userInsertPageMem(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "member/member_login_insert_mem";
+	}
+	
+	@GetMapping("/member/member_login_insert_com")
+	public String userInsertPageCom(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "member/member_login_insert_com";
+	}
+	
+	@GetMapping("/member/member_login_insert_pet")
+	public String userInsertPagePet(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "member/member_login_insert_pet";
+	}
 }
