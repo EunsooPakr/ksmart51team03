02c2899fdd9ad737deee62600683924872df883e
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-06-24 10:38:49 +0900

    작성자 : 송재익 작성일 : 10:38 작성내용 : 1. 예약 정보 및 결제 페이지 화면단 2. 페이지 경로 수정

--- pal/ksmart51team03/src/main/java/ks51team03/funeral/reserve/controller/ReserveController.java
+++ pal/ksmart51team03/src/main/java/ks51team03/funeral/reserve/controller/ReserveController.java
@@ -6,9 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReserveController {
 
-    @GetMapping("funeral/reserveDetail")
+    @GetMapping("funeral/funeral_reserve_detail")
     public String reserveDetail() {
 
         return "funeral/funeral_reserve_detail";
     }
+
+    @GetMapping("funeral/funeral_reserve_info")
+    public String reserveInfo() {
+
+        return "funeral/funeral_reserve_info";
+    }
+
+    @GetMapping("funeral/funeral_reserve_payment")
+    public String reservePayment() {
+
+        return "funeral/funeral_reserve_payment";
+    }
+
+    @GetMapping("funeral/funeral_reserve_confirm")
+    public String reserveConfirm() {
+
+        return "funeral/funeral_reserve_confirm";
+    }
+
+
 }
